68309a2f14ff4eb110904fbfd72a1a3ebc8eb916
yingalive <chenying9408@gmail.com>
2016-11-02 20:13:20 -0700

    new file:   data/a.csv 	new file:   data/data.csv 	deleted:    index.html 	deleted:    sample-apps 	new file:   server.R 	new file:   ui.R 	new file:   www/style2.css

---
+++ ui.R
@@ -0,0 +1,32 @@
+library(shinydashboard)
+
+header = dashboardHeader(title="Restaurants you may Like!",
+                        titleWidth =300)
+body = dashboardBody(
+  fluidRow(
+    column(width=4,
+           box(width=NULL, 
+                 selectInput("pick_user", label="User ID:",
+                             selected = "u1", multiple =F,
+                             choices = levels(review$user_id))
+               
+           ),
+           box(width=NULL,
+               htmlOutput("userinfo",height =400)
+           )
+    ),
+    column(width = 8,
+           box(width = NULL, solidHeader = TRUE,
+               htmlOutput("restinfo", height=400)
+               
+           )
+           
+    )
+  )
+)
+dashboardPage(
+  header,
+  dashboardSidebar(disable = TRUE),
+  body,
+  skin="red"
+)
